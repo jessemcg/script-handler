1cff39ce71c0d795a5ba92c26355796300fc501a
jessemcg <jessemcg@gmail.com>
2024-06-25 21:45:40 -0700

    changed sample

---
+++ scripts/SampleScript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+notify-send "This script has been executed successfully. You can also place scripts in directories to help keep them organized, and script-handler will present the directory name as an option."
+
--- scripts/Utilities/SampleScript.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python $HOME/script-handler/Sample_Python_App.py
